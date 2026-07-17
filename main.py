0bfc4e7913915a1900e9e60bfcac8472128197e2
Sourodip_18 <sourodipmahato762@gmail.com>
2026-07-17 18:49:10 +0530

    modifying the file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import qrcode
 
+# required_libraries = ["qrcode"]
+
 url =input("Enter the URL to generate QR code: ")
 file_path = "C:\\Users\\kouli\\OneDrive\\Desktop\\sourodip engineering\\qrcode.png"
 
